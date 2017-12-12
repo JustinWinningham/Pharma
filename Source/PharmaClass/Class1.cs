007b5d44f5d104f6d2dd35db149bdb865d5675a2
JustinWinningham <justinmwinningham@gmail.com>
2017-12-11 17:38:58 -0800

    Source Update
    Worked on Source a bit

--- Source/PharmaClass/Class1.cs
+++ Source/PharmaClass/Class1.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using UnityEngine;
+using Verse;
+using RimWorld;
+
 namespace Pharma
 {
-    public class PharmaClass
+    public class Building_PharmaVat : Building
     {
-        private string x;
-        
+        private enum Phase
+        {
+            offline,
+            active
+        }
+
+        private CompGlower glowerComp;
+        private CompPowerTrader powerComp;
+
+        private bool destroyedFlag = false;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+
+            SetPowerGlower();
+        }
     }
 }
